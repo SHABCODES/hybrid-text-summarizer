6191572fedb695f31efeb56ad77c6c836582bcce
SHABCODES <134513967+SHABCODES@users.noreply.github.com>
2025-10-19 13:59:03 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,10 @@
+"""
+Hybrid Text Summarization Package
+"""
 
+from .preprocessor import RobustTextPreprocessor
+from .summarizer import ImprovedHybridSummarizer
+
+__version__ = "1.0.0"
+__author__ = "Your Name"
+__all__ = ["RobustTextPreprocessor", "ImprovedHybridSummarizer"]
